cb62481c1b0db07986b637b7a970a09f5fc0bcc3
wy8881 <wy7382@gmail.com>
2023-10-28 19:18:30 +1030

    add check for ballot in frontend

--- frontend/src/components/Ballot.js
+++ frontend/src/components/Ballot.js
@@ -4,19 +4,92 @@ import './Ballot.css';
 export default function Ballot() {
     const [votes, setVotes] = useState({});
 
-    const handleVoteChange = (id, value) => {
+    function handleVoteChange (id, value) {
         setVotes({
             ...votes,
             [id]: value
         });
-    };
+    }
+
+    function handleError(message) {
+        window.alert(message);
+        setVotes({});
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if(checkCorrecrtMethod() && checkCorrectNumber()) {
+            console.log("submitted");
+        }
+    }
+
+    function checkCorrecrtMethod() {
+            let aboveCount = 0;
+            let belowCount = 0;
+
+            for(let key in votes) {
+                if(key.startsWith('above') && votes[key]) {
+                    aboveCount ++;
+                }
+                else if(key.startsWith('below') && votes[key]) {
+                    belowCount ++;
+                }
+            }
+            if(aboveCount > 0 && belowCount >0) {
+                handleError("You can only vote above or below the line");
+                return false;
+            }
+            return true;
+    }
+
+    function checkCorrectNumber() {
+        const voteValues = Object.values(votes).map(value => parseInt(value, 10));
+        // if(!voteValues.every(Number.isInteger)){
+        //     handleError("You can only vote with integers");
+        //     return false;
+        // }
+        const isAboveLine = Object.keys(votes).some(key => key.includes('above'));
+        const isBelowLine = Object.keys(votes).some(key => key.includes('below'));
+        const isUnique = (arr) => {
+            const uniqueNumbers = [...new Set(arr)];
+            return uniqueNumbers.length === arr.length
+        };
+
+        const minVote = Math.min(...voteValues);
+        const maxVote = Math.max(...voteValues);
+        const isConsecutive = maxVote - minVote === voteValues.length - 1;
+
+        if (isAboveLine){
+            if(!(isUnique(voteValues) && minVote === 1 && voteValues.length >= 6 && isConsecutive)) {
+                handleError("You have not voted correctly above the line. You need to number at least six boxes from 1 to 6.");
+                return false;
+        }
+
+        }
+
+
+        if (isBelowLine) {
+            if(!(isUnique(voteValues) && minVote === 1 && voteValues.length >= 12 && isConsecutive)) {
+                handleError("You have not voted correctly below the line. You need to number at least twelve boxes from 1 to 12.");
+                return false;
+            }
+        }
+
+
+        return true;
+    }
+
+    function checkValid() {
+
+
+    }
+
 
     return (
-        <div className="App">
+        <div className="Ballot">
             <h2>Senate Ballot Paper</h2>
             <p>You may vote in one of two ways</p>
             <h3>Either</h3>
-            <p>Above the line</p>
             <div className="above-line">
                 {Array.from({ length: 6 }).map((_, index) => (
                     <div key={index} className="party">
@@ -31,7 +104,6 @@ export default function Ballot() {
             </div>
 
             <h3>Or</h3>
-            <p>Below the line</p>
             <div className="below-line">
                 {Array.from({ length: 12 }).map((_, index) => (
                     <div key={index} className="candidate">
@@ -45,6 +117,7 @@ export default function Ballot() {
                     </div>
                 ))}
             </div>
+            <button className="submit-button" type="submit" onClick={handleSubmit}>Submit</button>
         </div>
     );
 }
