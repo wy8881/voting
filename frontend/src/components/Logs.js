9328f037b308d50d4c2b0fd910dea799ec3188b3
ABCDEEF <you@example.com>
2023-11-15 00:28:04 +1030

    fixed bugs in frontend: Download all logs.

--- frontend/src/components/Logs.js
+++ frontend/src/components/Logs.js
@@ -54,8 +54,21 @@ const  Logs = () => {
     // download all logs
     async function onDownloadAllLogs() {
         await api.get('api/logs/all').then((response) => {
+            // convret recived data to list of strings
+            let log_list = [];
+
+            response.data.forEach((log) => {
+                log_list.push(log.username + " " + log.action + " " + log.id.date);
+            });
+
+            // convert list of strings to string
+            let log_string = "";
+            log_list.forEach((log) => {
+                log_string += log + "\n";
+            });
+
             const element = document.createElement("a");
-            const file = new Blob([response.data], {type: 'text/plain'});
+            const file = new Blob([log_string], {type: 'text/plain'});
             element.href = URL.createObjectURL(file);
             element.download = "logs.txt";
             document.body.appendChild(element); // Required in FireFox
